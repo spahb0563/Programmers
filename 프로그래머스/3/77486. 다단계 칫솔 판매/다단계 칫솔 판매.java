8ea8221c1a60d89de20e840cf34579e6c16ceaa2
문성준 <102467004+spahb0563@users.noreply.github.com>
2024-08-04 00:51:50 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 4678.70 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -3,20 +3,13 @@ import java.util.*;
 class Solution {
     
     static class Node {
-        String name;
-    
         int profits;
         
         Node parent;
         
-        Node(String name, Node parent) {
-            this.name = name;
+        Node(Node parent) {
             this.parent = parent;
         }   
-        
-        public String getName() {
-            return this.name;
-        }
         
         public Node getParent() {
             return this.parent;
@@ -34,23 +27,21 @@ class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] sellers, int[] amount) {
         int[] answer = new int[enroll.length];
         
-        Node center = new Node("-", null);
+        Node center = new Node(null);
         
         Map<String, Node> map = new HashMap<>();
         
-        map.put(center.getName(), center);
+        map.put("-", center);
         
         for(int i = 0 ; i < enroll.length; i ++) {
-            Node node = new Node(enroll[i], map.get(referral[i]));
-            map.put(node.getName(), node);
+            Node node = new Node(map.get(referral[i]));
+            map.put(enroll[i], node);
         }
-        
         
         for(int i = 0 ; i < sellers.length; i ++) {
             Node child = map.get(sellers[i]);
             
             int money = amount[i] * 100;
-            
             
             while(child.getParent() != null) {
                 int giveToParent = (int)(money * 0.1);
