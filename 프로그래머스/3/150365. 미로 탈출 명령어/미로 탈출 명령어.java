a4094c9c49ae88dcb031e0b9efe0713fc82d4b87
문성준 <102467004+spahb0563@users.noreply.github.com>
2024-09-05 23:14:24 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.66 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -11,9 +11,7 @@ class Solution {
     static String answer;
     
     static void dfs(char[] arr, int x, int y, int z, int depth) {
-        if(!answer.equals("")) return;
-        
-        if(depth > z) return;
+        if(answer != null) return;
         
         if(depth == z) {
             if(x == r && y == c) {
@@ -25,8 +23,6 @@ class Solution {
         for(int i = 0 ; i < 4 ; i ++) {
             int newX = x + coor[i][0];
             int newY = y + coor[i][1];
-            if(depth + 1 > z) continue;
-            
             if(newX >= 0 && newY >= 0 && newY < m && newX < n) {
                 if(distance[newX][newY] > z - depth) continue;
                 arr[depth] = ch[i];
@@ -36,7 +32,7 @@ class Solution {
     }
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        answer = "";
+        answer = null;
         
         this.r = r - 1;
         this.c = c - 1;
@@ -47,7 +43,7 @@ class Solution {
         
         for(int i = 0 ; i < n ; i ++) {
             for(int j = 0 ; j < m ; j ++) {
-                distance[i][j] = Math.max(i, r-1) - Math.min(i, r-1) + Math.max(j, c-1) - Math.min(j, c-1); 
+                distance[i][j] = Math.abs(i - (r - 1)) + Math.abs(j - (c - 1));
             }
         }
         
@@ -56,8 +52,6 @@ class Solution {
         if(distance[x-1][y-1] % 2 != k % 2) return "impossible";
         
         dfs(new char[k], x-1, y-1, k, 0);
-        
-        if(answer.equals("")) answer = "impossible";
         
         return answer;
     }
