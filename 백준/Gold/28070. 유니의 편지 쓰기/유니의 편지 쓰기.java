88b1250ae427590aa4931b2a9e47dd47a9ebc5cc
문성준 <102467004+spahb0563@users.noreply.github.com>
2024-09-07 18:28:45 +0900

    [Gold V] Title: 유니의 편지 쓰기, Time: 440 ms, Memory: 76656 KB -BaekjoonHub

---
+++ 백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int toIdx(String date) {
+		String[] split = date.split("-");
+		
+		int year = Integer.parseInt(split[0]);
+		
+		int month = Integer.parseInt(split[1]);
+		
+		int idx = ((year - 2000) * 12) + (month - 1);
+		
+		return idx;
+	}
+	
+	static String toDate(int idx) {
+		int year = 2000 + (idx / 12);
+		
+		int month = (idx % 12) + 1;
+		
+		return String.format("%4d-%02d", year, month);
+	}
+	
+    public static void main(String[] args) throws Exception{
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	int[] arr = new int[((9999 - 1999) * 12) + 1];
+    	
+    	for(int i = 0 ; i < N ; i ++) {
+    		String line = br.readLine();
+    		
+    		String from = line.substring(0, 7);
+    		
+    		String to = line.substring(8, line.length());
+    		
+    		int fromIdx = toIdx(from); 
+    		int toIdx = toIdx(to); 
+    		arr[fromIdx]++;
+    		arr[toIdx + 1]--;
+    	}
+    	
+    	int max = arr[0];
+    	int maxIdx = 0;
+    	for(int i = 1 ; i < arr.length - 1; i ++) {
+    		arr[i] = arr[i] + arr[i-1];
+    		
+    		if(max < arr[i]) {
+    			max = arr[i];
+    			maxIdx = i;
+    		}
+    		
+    	}
+    	
+    	System.out.println(toDate(maxIdx));
+    	br.close();
+    }
+}
